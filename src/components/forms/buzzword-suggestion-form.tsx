2e7683fef26b0d60e5864f7bb9fd0f336882ce45
gojutin <gojutin@gmail.com>
2019-08-14 19:29:39 -0400

    Testing Netlify form with modal.

--- src/components/forms/buzzword-suggestion-form.tsx
+++ src/components/forms/buzzword-suggestion-form.tsx
@@ -24,87 +24,87 @@ const NetlifyFormComponent: React.FC<NetlifyFormProps> = () => {
       >
         Suggest a Buzzword
       </Button>
-      <Modal
+      {/* <Modal
         title="Buzzword Suggestion"
         visible={isOpen}
         forceRender
         footer={null}
         onCancel={() => setIsOpen(false)}
         maskStyle={{ background: "rgba(0,0,0,.8)" }}
+      > */}
+      <div
+        css={`
+          max-width: 100%;
+        `}
       >
-        <div
-          css={`
-            max-width: 100%;
-          `}
-        >
-          <NetlifyForm name="Buzzword Suggestion">
-            {({ loading, error, success }) => {
-              if (success) {
-                return (
-                  <h3>
-                    <Emoji label="Thumbs up" symbol="👍" size={20} />
-                    Thanks for the suggestion!
-                  </h3>
-                );
-              } else if (error) {
-                return (
-                  <h3
-                    css={`
-                      color: ${props => props.theme.red};
-                    `}
-                  >
-                    Oops..something went wrong. Please try again.
-                  </h3>
-                );
-              } else if (!success && !error) {
-                return (
-                  <>
-                    <label htmlFor="word">
-                      Word
-                      <Input
-                        name="word"
-                        id="word"
-                        autoFocus
-                        required
-                        type="text"
-                      />
-                    </label>
-                    <label htmlFor="definition">
-                      Definition
-                      <Input
-                        required
-                        name="definition"
-                        id="definition"
-                        type="text"
-                      />
-                    </label>
+        <NetlifyForm name="Buzzword Suggestion">
+          {({ loading, error, success }) => {
+            if (success) {
+              return (
+                <h3>
+                  <Emoji label="Thumbs up" symbol="👍" size={20} />
+                  Thanks for the suggestion!
+                </h3>
+              );
+            } else if (error) {
+              return (
+                <h3
+                  css={`
+                    color: ${props => props.theme.red};
+                  `}
+                >
+                  Oops..something went wrong. Please try again.
+                </h3>
+              );
+            } else if (!success && !error) {
+              return (
+                <>
+                  <label htmlFor="word">
+                    Word
+                    <Input
+                      name="word"
+                      id="word"
+                      autoFocus
+                      required
+                      type="text"
+                    />
+                  </label>
+                  <label htmlFor="definition">
+                    Definition
+                    <Input
+                      required
+                      name="definition"
+                      id="definition"
+                      type="text"
+                    />
+                  </label>
 
-                    <label htmlFor="submitter-name">
-                      Your Name
-                      <Input
-                        name="submitter-name"
-                        id="submitter-name"
-                        type="text"
-                      />
-                    </label>
-                    <br />
-                    <Button
-                      css={`
-                        margin-top: 10px;
-                      `}
-                      htmlType="submit"
-                      type="primary"
-                      loading={loading}
-                    >
-                      Submit
-                    </Button>
-                  </>
-                );
-              } else return null;
-            }}
-          </NetlifyForm>
-        </div>
-      </Modal>
+                  <label htmlFor="submitter-name">
+                    Your Name
+                    <Input
+                      name="submitter-name"
+                      id="submitter-name"
+                      type="text"
+                    />
+                  </label>
+                  <br />
+                  <Button
+                    css={`
+                      margin-top: 10px;
+                    `}
+                    htmlType="submit"
+                    type="primary"
+                    loading={loading}
+                  >
+                    Submit
+                  </Button>
+                </>
+              );
+            } else return null;
+          }}
+        </NetlifyForm>
+      </div>
+      {/* </Modal> */}
     </>
   );
 };
