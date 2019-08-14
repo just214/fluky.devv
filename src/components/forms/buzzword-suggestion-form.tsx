dd2931b3360f8f371167e35c0edcdd030d050191
gojutin <gojutin@gmail.com>
2019-08-14 16:00:04 -0400

    Testing netlify form.

--- src/components/forms/buzzword-suggestion-form.tsx
+++ src/components/forms/buzzword-suggestion-form.tsx
@@ -1,30 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "antd/es/button";
 import Input from "antd/es/input";
-import NetlifyForm from "../common/netlify-form";
+import NetlifyForm from "react-netlify-form";
+import Modal from "antd/es/modal";
+import { Emoji } from "../common";
 
-const BuzzwordSuggestionForm = () => {
+interface NetlifyFormProps {
+  formName: string;
+  buttonText: string;
+  buttonIcon?: string;
+  successMessage: string;
+}
+
+const NetlifyFormComponent: React.FC<NetlifyFormProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <NetlifyForm
-      formName="Buzzword Suggestion"
-      buttonText="Suggest a Buzzword"
-      successMessage="Thanks for the suggestion!"
-    >
-      <input type="hidden" name="form-name" value="Buzzword Suggestion" />
-      <label htmlFor="word">
-        Word
-        <Input name="word" id="word" autoFocus required type="text" />
-      </label>
-      <label htmlFor="definition">
-        Definition
-        <Input required name="definition" id="definition" type="text" />
-      </label>
+    <>
+      <Button
+        size="large"
+        type="link"
+        onClick={() => setIsOpen(true)}
+        style={{ margin: 0, padding: 0 }}
+      >
+        Suggest a Buzzword
+      </Button>
+      <Modal
+        title="Buzzword Suggestion"
+        visible={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        maskStyle={{ background: "rgba(0,0,0,.8)" }}
+      >
+        <div
+          css={`
+            max-width: 100%;
+          `}
+        >
+          <NetlifyForm name="Buzzword Suggestion">
+            {({ loading, error, success }) => {
+              if (success) {
+                return (
+                  <h3>
+                    <Emoji label="Thumbs up" symbol="👍" size={20} />
+                    Thanks for the suggestion!
+                  </h3>
+                );
+              } else if (error) {
+                return (
+                  <h3
+                    css={`
+                      color: ${props => props.theme.red};
+                    `}
+                  >
+                    Oops..something went wrong. Please try again.
+                  </h3>
+                );
+              } else if (!success && !error) {
+                return (
+                  <div>
+                    <input
+                      type="hidden"
+                      name="form-name"
+                      value="Buzzword Suggestion"
+                    />
+                    <label htmlFor="word">
+                      Word
+                      <Input
+                        name="word"
+                        id="word"
+                        autoFocus
+                        required
+                        type="text"
+                      />
+                    </label>
+                    <label htmlFor="definition">
+                      Definition
+                      <Input
+                        required
+                        name="definition"
+                        id="definition"
+                        type="text"
+                      />
+                    </label>
 
-      <label htmlFor="submitter-name">
-        Your Name
-        <Input name="submitter-name" id="submitter-name" type="text" />
-      </label>
-    </NetlifyForm>
+                    <label htmlFor="submitter-name">
+                      Your Name
+                      <Input
+                        name="submitter-name"
+                        id="submitter-name"
+                        type="text"
+                      />
+                    </label>
+                    <br />
+                    <Button
+                      css={`
+                        margin-top: 10px;
+                      `}
+                      htmlType="submit"
+                      type="primary"
+                      loading={loading}
+                    >
+                      Submit
+                    </Button>
+                  </div>
+                );
+              } else return null;
+            }}
+          </NetlifyForm>
+        </div>
+      </Modal>
+    </>
   );
 };
 
-export default BuzzwordSuggestionForm;
+export default NetlifyFormComponent;
