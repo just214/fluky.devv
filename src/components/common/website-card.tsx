afb7634cf6096ed2eefd42b03666ed201ba79c9f
gojutin <gojutin@gmail.com>
2019-08-15 00:41:28 -0400

    Refactor of website card.

--- src/components/common/website-card.tsx
+++ src/components/common/website-card.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import { FaImage } from "react-icons/fa";
+import Divider from "antd/es/divider";
 
 const Box = styled.div`
-  border: 1px solid #dadada;
-  border-radius: 10px;
+  /* border: 1px solid #dadada;
+  border-radius: 10px; */
   display: flex;
   align-items: center;
   margin: 5px 0px;
-  padding: 5px;
+  padding: 10px;
+  @media (max-width: 500px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `;
 
 export interface WebsiteCardProps {
@@ -16,28 +21,48 @@ export interface WebsiteCardProps {
 }
 
 export const WebsiteCard: React.FC<WebsiteCardProps> = ({ data }) => {
-  console.log(data);
   return (
-    <Box>
-      <div
-        css={`
-          margin-right: 8px;
-        `}
-      >
-        {data.image ? (
-          <img width={100} src={data.image} alt={data.description} />
-        ) : (
-          <FaImage size={100} />
-        )}
-      </div>
-      <div>
-        <h3>{data.title}</h3>
-        <p>{data.description}</p>
-        <a href={data.url} target="_blank" rel="noopener noreferrer">
-          {data.url}
-        </a>
-      </div>
-    </Box>
+    <>
+      <Box>
+        <div
+          css={`
+            margin-right: 12px;
+            @media (max-width: 500px) {
+              margin-right: 0px;
+              margin-bottom: 8px;
+            }
+          `}
+        >
+          {data.image ? (
+            <img width={100} src={data.image} alt={data.description} />
+          ) : (
+            <FaImage size={100} color="#efefef" />
+          )}
+        </div>
+        <div>
+          <h3
+            css={`
+              margin-bottom: 5px;
+            `}
+          >
+            {data.title}{" "}
+            <small
+              css={`
+                color: ${props => props.theme.gray4};
+              `}
+            >
+              by {data.provider}
+            </small>
+          </h3>
+
+          <p>{data.description}</p>
+          <a href={data.url} target="_blank" rel="noopener noreferrer">
+            {data.url}
+          </a>
+        </div>
+      </Box>
+      <Divider />
+    </>
   );
 };
 
